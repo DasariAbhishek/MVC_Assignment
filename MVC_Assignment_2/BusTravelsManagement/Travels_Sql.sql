04a134dfaa41c8d744c96d001a2c4509a27ab4f2
DasariAbhishek <99283204+DasariAbhishek@users.noreply.github.com>
2022-08-24 12:20:31 +0530

    Add files via upload

---
+++ MVC_Assignment_2/BusTravelsManagement/Travels_Sql.sql
@@ -0,0 +1,40 @@
+CREATE TABLE BusInfo
+(
+	BusID INT Identity(1,1) PRIMARY KEY,
+	BoardingPoint NVARCHAR(3),
+	TravelDate DATE,
+	Amount DECIMAL(5,2),
+	Rating INT
+);
+
+INSERT	INTO BusInfo(BoardingPoint, TravelDate, Amount, Rating)
+		VALUES	('BGL',	'2017-06-18',	400.65,	2),
+				('HYD',	'2017-10-05',	600.00,	3),
+				('CHN',	'2016-07-25',	445.95,	3),
+				('PUN',	'2017-12-10',	543.00,	4),
+				('MUM',	'2017-01-28',	500.50,	4),
+				('PUN',	'2016-03-27',	333.55,	3),
+				('MUM',	'2016-11-06',	510.00,	4);
+
+SELECT * FROM BusInfo;
+
+CREATE PROCEDURE spInsertBusInfo
+@BoardingPoint NVARCHAR(3),
+@TravelDate DATE,
+@Amount DECIMAL(5,2),
+@Rating INT
+AS
+BEGIN
+	INSERT	INTO BusInfo(BoardingPoint, TravelDate, Amount, Rating)
+		VALUES	(@BoardingPoint,	@TravelDate,	@Amount,	@Rating);
+END;
+
+--Creating views 
+
+CREATE VIEW Bus_View
+AS
+	SELECT BusID, BoardingPoint
+	FROM BusInfo
+	WHERE Rating > 3;
+
+SELECT * FROM Bus_View;
